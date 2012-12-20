b7bf1c1a4346322f3a79810bf67ddd09ed48121f
lemanyk <zloc@ya.ru>
2012-12-20 18:31:18 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,20 @@ from distutils.core import setup
 
 setup(
     name='netlog',
-    version='0.1',
+    version='0.2',
     py_modules=['netlog'],
     url='https://github.com/lemanyk/netlog',
+    download_url='https://github.com/lemanyk/netlog/',
     description='logging stream server written on gevent',
     author='Gennady Leman',
     author_email='lemanyk@gmail.com',
     install_requires=['gevent'],
+    license='MIT',
+    classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+    ],
 )
