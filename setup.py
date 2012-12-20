df3bc9890ffad5a078bac4c9a61dfe1b0eb16697
lemanyk <zloc@ya.ru>
2012-12-20 17:07:02 +0400

    register in pypi

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ from distutils.core import setup
 setup(
     name='netlog',
     version='0.1',
-    packages=['netlog'],
+    py_modules=['netlog'],
+    url='https://github.com/lemanyk/netlog',
+    description='logging stream server written on gevent',
+    author='Gennady Leman',
+    author_email='lemanyk@gmail.com',
+    install_requires=['gevent'],
 )
